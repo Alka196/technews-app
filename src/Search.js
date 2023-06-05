b80792555d5eedcda65a2a246e12f5b35c0e6e14
Alka Chaudhary <alkawp19@gmail.com>
2023-06-06 00:45:53 +0530

    6-6-f1

--- src/Search.js
+++ src/Search.js
@@ -7,7 +7,9 @@ function Search() {
 
     return (
         <>
-            <h1> APNA Tech NEWS </h1>
+           <h1><span><img style={{height:"90px",width:"120px"}}
+           src="https://w7.pngwing.com/pngs/174/828/png-transparent-newspaper-animation-blackboard-newspaper-love-text-logo.png"
+            alt="logo"/> </span> &nbsp;APNA Tech NEWS </h1>
             <form onSubmit={(e) => e.preventDefault()}>
                 <div>
                     <input
