a6149e5ab3b9066f1abc359a271aad412239c7cf
Alka Chaudhary <alkawp19@gmail.com>
2023-07-13 14:05:08 +0530

    13-7

--- src/Search.js
+++ src/Search.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useGlobalContext } from './Context';
+import './Search.css'
+
 
 function Search() {
 
     const { query, searchPost } = useGlobalContext();
+    const [email, setEmail] = useState('');
+
+    const handleSubscribe = (e) => {
+        e.preventDefault();
+        // Perform subscription logic here (e.g., sending email to server)
+        console.log('Subscribed:', email);
+        setEmail('');
+    };
 
     return (
         <>
-           <h1><span><img style={{height:"90px",width:"120px"}}
-           src="https://w7.pngwing.com/pngs/174/828/png-transparent-newspaper-animation-blackboard-newspaper-love-text-logo.png"
-            alt="logo"/> </span> &nbsp;APNA Tech NEWS </h1>
+        <form >
+            <div className='subs-btn'>
+                    <input
+                        className="email-input"
+                        type="email"
+                        placeholder="Enter your email address"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <button className="subscribe-button" type="submit">Subscribe Now</button>
+                    </div>
+                    </form>
+            <h1><span><img style={{ height: "90px", width: "120px" }}
+                src="https://w7.pngwing.com/pngs/174/828/png-transparent-newspaper-animation-blackboard-newspaper-love-text-logo.png"
+                alt="logo" /> </span> &nbsp;APNA Tech NEWS </h1>
+
             <form onSubmit={(e) => e.preventDefault()}>
+
                 <div>
                     <input
                         type='text'
@@ -18,9 +42,10 @@ function Search() {
                         value={query}
                         onChange={(e) => searchPost(e.target.value)}
                     />
-
-                </div>
+                    </div>
+                   
             </form>
+            
 
         </>
 
